f8c940e60c7bde0714429b9338219d7634b0dc86
nikitadubino <nikita.dubino@softswiss.com>
2024-01-17 16:05:44 +0400

    test_make_a_post

--- tests/feature_buzz_test.py
+++ tests/feature_buzz_test.py
@@ -9,7 +9,7 @@ from base.base_test import BaseTest
 class TestNewsfeedFeature(BaseTest):
     @allure.title("Post a buzz")
     @allure.severity("Normal")
-    @pytest.mark.critical
+    @pytest.mark.smoke
     def test_make_a_post(self):
         self.login_page.open()
         self.login_page.enter_login(self.data.LOGIN)
@@ -21,15 +21,15 @@ class TestNewsfeedFeature(BaseTest):
         self.buzz_page.post_buzz()
         self.buzz_page.is_changes_saved()
 
-    # @allure.title("Delete a buzz")
-    # @allure.severity("Normal")
-    # @pytest.mark.critical
-    # def test_delete_a_post(self):
-    #     self.login_page.open()
-    #     self.login_page.enter_login(self.data.LOGIN)
-    #     self.login_page.enter_password(self.data.PASSWORD)
-    #     self.login_page.click_submit_button()
-    #     self.dashboard_page.is_opened()
-    #     self.dashboard_page.click_buzz_link()
-    #     self.buzz_page.is_opened()
-    #     self.buzz_page.delete_buzz()
+    @allure.title("Delete a buzz")
+    @allure.severity("Normal")
+    @pytest.mark.smoke
+    def test_delete_a_post(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_submit_button()
+        self.dashboard_page.is_opened()
+        self.dashboard_page.click_buzz_link()
+        self.buzz_page.is_opened()
+        self.buzz_page.delete_buzz()
